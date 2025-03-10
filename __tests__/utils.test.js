c8ea078478bef004e40c42e1ca019a76ff652745
CCronje96 <chantellec.github@gmail.com>
2025-03-10 15:59:52 +0000

    added /api/articles endpoint, error-handling and completed testing

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -598,3 +598,12 @@ describe("formatComments", () => {
     });
   });
 });
+
+exports.checkExists = async (table, column, value) => {
+  const queryStr = format("SELECT * FROM %I WHERE %I = $1;", table, column);
+  const dbOutput = await db.query(queryStr, [value]);
+
+  if (dbOutput.rows.length === 0) {
+    return Promise.reject({ code: 404, message: "not found" });
+  }
+};
